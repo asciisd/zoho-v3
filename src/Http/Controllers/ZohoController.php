23d830edaedd385107d23c33f592b5835284df3d
aemaddin <aemaddin@gmail.com>
2022-10-18 00:18:32 +0200

    - wip

--- src/Http/Controllers/ZohoController.php
+++ src/Http/Controllers/ZohoController.php
@@ -4,17 +4,13 @@ namespace Asciisd\Zoho\Http\Controllers;
 
 use Asciisd\Zoho\Zoho;
 use Illuminate\Routing\Controller;
-use com\zoho\crm\api\exception\SDKException;
 use Asciisd\Zoho\Http\Requests\ZohoRedirectRequest;
 
 class ZohoController extends Controller
 {
     public function oauth2callback(ZohoRedirectRequest $request)
     {
-        try {
-//            Zoho::initialize($request->code);
-        } catch (SDKException $e) {
-        }
+        Zoho::initialize($request->code);
 
         return 'Zoho CRM has been set up successfully.';
     }
