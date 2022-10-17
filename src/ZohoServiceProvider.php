d1b4861d097d8de7d21b17c083b8ce87da0d710c
aemaddin <aemaddin@gmail.com>
2022-10-18 00:11:51 +0200

    - wip

--- src/ZohoServiceProvider.php
+++ src/ZohoServiceProvider.php
@@ -17,11 +17,11 @@ class ZohoServiceProvider extends PackageServiceProvider
          *
          * More info: https://github.com/spatie/laravel-package-tools
          */
-        $package
-            ->name('zoho-v3')
-            ->hasCommands(ZohoAuthentication::class, ZohoInstallCommand::class, ZohoSetupCommand::class)
-            ->hasConfigFile('zoho')
-            ->hasRoute('web')
-            ->hasMigration('create_zohos_table');
+//        $package
+//            ->name('zoho-v3')
+//            ->hasCommands(ZohoAuthentication::class, ZohoInstallCommand::class, ZohoSetupCommand::class)
+//            ->hasConfigFile('zoho')
+//            ->hasRoute('web')
+//            ->hasMigration('create_zohos_table');
     }
 }
